0274974a4a8046a9a6d3d6e9317be9f56adf5dc8
zhan849 <zhan849@usc.edu>
2013-12-02 16:55:08 -0800

    integrate test

---
+++ client/scripts/integrate_main.js
@@ -0,0 +1,70 @@
+(function (requirejs) {
+  "use strict";
+
+  requirejs.config({
+    shim: {
+      'lib/jquery': {
+        exports: 'jQuery'
+      },
+      'lib/jquery.ui.core': {
+        deps: ['lib/jquery']
+      },
+      'lib/jquery.ui.widget': {
+        deps: ['lib/jquery.ui.core']
+      },
+      'lib/jquery.ui.position': {
+        deps: ['lib/jquery.ui.core']
+      },
+      'lib/jquery.ui.autocomplete': {
+        deps: ['lib/jquery.ui.core', 'lib/jquery.ui.widget', 'lib/jquery.ui.position']
+      },
+      'lib/jvent': {
+        exports: 'jvent'
+      },
+      'lib/jplayer.min': {
+        deps: ['lib/jquery']
+      },
+      'lib/prefixfree.jquery': {
+        deps: ['lib/prefixfree.min']
+      },
+	  'eic/pluginsniff':{
+		exports: 'pluginsniff'
+	  },
+	  'lib/base64_handler':{
+		exports: 'base64_handler'
+	  },	  
+    },
+  });
+
+	require(['eic/PresentationController', 'eic/PresentationController2','eic/PiecesUI'], function(PresentationController, PresentationController2, PiecesUI){
+		var jsonObject,view;
+	    $.getJSON("../data_json/hash_object_test_1.json", function(data){
+			jsonObject = data;
+			var controller = new PresentationController(jsonObject);
+			view = new PiecesUI(controller);
+			controller.init();
+            view.init();
+			console.log("Hash Object Output", jsonObject);
+        		//view = new PiecesUI(controller);
+			// var controller = new PresentationController(jsonObject);
+			// view = new PiecesUI(controller);
+			// controller.makeMovie();
+			// controller.init();
+			// view.init();
+			
+			
+		});
+		
+		$("#generateVideoEditor").click(function(){
+			console.log("Hash Object Output", jsonObject);
+		});
+			
+	    //var controller = new PresentationController(jsonObject);
+	      //  view = new PiecesUI(controller);
+	        //controller.makeMovie();
+	    //controller.init();
+	    //view.init();
+	});
+ 
+})(requirejs);
+
