808ea8cbd580c972252370ba6180a8065046a7dc
Linda Xu <lindaxu@usc.edu>
2014-01-26 14:54:54 -0800

    Reverted TTSService again...

--- client/scripts/eic/TTSService.js
+++ client/scripts/eic/TTSService.js
@@ -13,10 +13,10 @@
 define(['lib/jquery', 'eic/Logger', 'lib/jvent', 'config/URLs', 'lib/base64_handler'],
   function ($, Logger, EventEmitter, urls) {
     "use strict";
-    var logger = new Logger("TTSService");  
+    var logger = new Logger("TTSService");
     
     //IE is the only current browser without data: uri support, so check if it supports Blobs...otherwise we must wait for synthesis during the embedding phase...
-    /*var urlType;
+    var urlType;
     if (navigator.userAgent.indexOf('MSIE') !=-1){
                 if (window.URL.createObjectURL)
                         urlType=1; //Since comparing strings is hard, let's use integers: 1=objectURL 2=normal URL 3=data:uri
@@ -27,7 +27,7 @@ define(['lib/jquery', 'eic/Logger', 'lib/jvent', 'config/URLs', 'lib/base64_hand
                         urlType=1;
         else
             urlType=3;
-      */                     
+                           
 
     function TTSService() {
       EventEmitter.call(this);
@@ -43,74 +43,74 @@ define(['lib/jquery', 'eic/Logger', 'lib/jvent', 'config/URLs', 'lib/base64_hand
         sendSpeech();
         
          function sendSpeech(){
-			if (self.finished)
-				return;
-			
-			self.attempt++;
-			
-			if (self.attempt==5){
-				self.finished = true;
-				logger.log('Error receiving speech (timed out)', text);
-				self.emit('speechError', text);
-				return;
-			}
-			 
-			setTimeout(function(){	
-				if (!self.finished){
-					self.attempt++;
-					sendSpeech(self.attempt);
-				}
-			},5000);
-			 
-			 $.ajax({
-				 url: urls.speech,
-				 type: 'GET',
-				 data: { req_text: text},
-				 dataType: 'jsonp',
-				 success: function (data) {					 
-					 												
-					 if (data.res === 'OK') {
-						self.finished = true;
-						
-						/*if (urlType==3) //data:uri method
-							data.snd_url+=data.text;
+                        if (self.finished)
+                                return;
+                        
+                        self.attempt++;
+                        
+                        if (self.attempt==5){
+                                self.finished = true;
+                                logger.log('Error receiving speech (timed out)', text);
+                                self.emit('speechError', text);
+                                return;
+                        }
+                        
+                        setTimeout(function(){        
+                                if (!self.finished){
+                                        self.attempt++;
+                                        sendSpeech(self.attempt);
+                                }
+                        },5000);
+                        
+                         $.ajax({
+                                 url: urls.festivalspeech,
+                                 type: 'GET',
+                                 data: { req_text: text, url_type: urlType},
+                                 dataType: 'jsonp',
+                                 success: function (data) {                                        
+                                                                                                                                         
+                                         if (data.res === 'OK') {
+                                                self.finished = true;
+                                                
+                                                if (urlType==3) //data:uri method
+                                                        data.snd_url+=data.text;
                         else if (urlType == 1){ //createObjectURL method...only available for new browsers
                             var blob = new Blob([base64DecToArr(data.text)], {type: "audio/wav"});
                             data.snd_url = window.URL.createObjectURL(blob);
                         }
                         else //Slow url method...just for IE 9 and under
-                            data.snd_url+=window.escape(data.text);*/
-						
-						logger.log('Received audio URL', text + 'url:' + data.snd_url);
-							
-						if (callback)
-							callback(data);
-						
-						self.emit('speechReady', data);
-					 }
-					 else {
-						if (self.attempt==4){
-							logger.log('Error receiving speech1', data);
-							self.emit('speechError', data);
-						}
-						else{
-							logger.log('Error with speech, new attempt: ' + self.attempt);
-							sendSpeech();
-						}
-					 }
-				 },
-				 error: function (error) {
-					if (attempt==4){
-						logger.log('Error receiving speech2', error);
-						self.emit('speechError', error);
-					}
-					else{
-						logger.log('Error with speech2, new attempt: ' + self.attempt);
-							sendSpeech();
-					}
-				 }
-			 });
-		}
+                            data.snd_url+=window.escape(data.text);
+                                                
+                                                logger.log('Received audio URL', text + 'url:' + data.snd_url);
+                                                        
+                                                if (callback)
+                                                        callback(data);
+                                                
+                                                self.emit('speechReady', data);
+                                         }
+                                         else {
+                                                if (self.attempt==4){
+                                                        logger.log('Error receiving speech1', data);
+                                                        self.emit('speechError', data);
+                                                }
+                                                else{
+                                                        logger.log('Error with speech, new attempt: ' + self.attempt);
+                                                        sendSpeech();
+                                                }
+                                         }
+                                 },
+                                 error: function (error) {
+                                        if (attempt==4){
+                                                logger.log('Error receiving speech2', error);
+                                                self.emit('speechError', error);
+                                        }
+                                        else{
+                                                logger.log('Error with speech2, new attempt: ' + self.attempt);
+                                                        sendSpeech();
+                                        }
+                                 }
+                         });
+                }
       }
     };
 
