55404a9ee6281cac4b92a15cedf0950a16946e5c
Harry Zhang <zhan849@usc.edu>
2014-01-28 20:47:31 -0800

    integrate with canvas, started up with another main.js inside eic, and created another PathFinder class

---
+++ client/scripts/eic/PathFinder.js
@@ -0,0 +1,69 @@
+define(['lib/jquery', 'eic/Logger', 'lib/d3'],
+  function ($, Logger, d3) {
+    "use strict";
+    var logger = new Logger("PathFinder");
+  		
+    function PathFinder(generator, path, controller, hashObj) {
+    	/* Define HTML Elements*/
+    	this.chartHTML = '<div id="chart">';
+			 	chartHTML += '<div id="nodeInfo">';
+				chartHTML += '<h3 id="info">Information</h3>';
+				chartHTML += '<p class = "infoContent" id="name"> </p>';
+				chartHTML += '<p class = "infoContent" id="catalog"> </p>';
+				chartHTML += '<img id="stickyNote" src="img/stickyNotes.png" />';
+				chartHTML += '</div></div>';
+		
+		this.searchBoxHTML = '<div id="searchBox">';
+				searchBoxHTML += '<h1>Linked Data Education</h1>';
+				searchBoxHTML += '<input id="input" />';
+				searchBoxHTML += '<button id="draw">Draw</button>';
+				searchBoxHTML += '</div>';
+		
+		this.stickyNote ='<div id="nodeInfo">';
+				stickyNote +='<h3 id="info">Information</h3>';
+				stickyNote +='<p class = "infoContent" id="name"> </p>';
+				stickyNote +='<p class = "infoContent" id="catalog"> </p>';
+				stickyNote +='<img id="stickyNote" src="img/stickyNotes.png" />';
+				stickyNote +='</div>';
+				
+		this.redraw = '<button id="redraw">Redraw</button>';
+		this.finish = '<button id="finish">Finish</button>';	    		
+		this.relationBox = '<div id="relation" style="display: none;"></div>';
+		
+		this.URLRef = new Object;
+				URLRef['Bill Clinton'] = "http%3A%2F%2Fdbpedia.org%2Fresource%2FBill_Clinton";
+				URLRef['Hillary Clinton'] = "http%3A%2F%2Fdbpedia.org%2Fresource%2F";
+				URLRef['Ray Bradbury'] = "http%3A%2F%2Fdbpedia.org%2Fresource%2F";
+				
+				
+		/* Define Constants and Data */
+		this.w = 150;
+    	this.h = 580;
+    	this.i = 0;
+    	this.duration = 500;
+    	this.root;
+    	this.appendList = [];
+		
+    }
+
+    /* Member functions */
+    PathFinder.prototype = {
+      // Starts the movie about the connection between the user and the topic.
+      startEdit: function () {},
+
+		
+    	grabMovieNav: function(){
+    		var movieNav = $("#movie-nav-bar .nodeElementBarContent");
+    		console.log(movieNav);
+    		var navlist = [movieNav.length];
+    		 for (var i = 0; i<movieNav.length; i++){
+    			 navlist[i] = movieNav[i].src;
+    		 }
+    		console.log("Movie Nav: ", navlist);
+    		this._Play_Sequence = navlist;
+    	}
+      
+    };
+
+    return PathFinder;
+  });
---
+++ client/scripts/linked_data_main.js
@@ -0,0 +1,59 @@
+(function (requirejs) {
+  "use strict";
+
+  requirejs.config({
+    shim: {
+      'lib/jquery': {
+        exports: 'jQuery'
+      },
+      'lib/jquery.ui.core': {
+        deps: ['lib/jquery']
+      },
+      'lib/jquery.ui.widget': {
+        deps: ['lib/jquery.ui.core']
+      },
+      'lib/jquery.ui.position': {
+        deps: ['lib/jquery.ui.core']
+      },
+      'lib/jquery.ui.autocomplete': {
+        deps: ['lib/jquery.ui.core', 'lib/jquery.ui.widget', 'lib/jquery.ui.position']
+      },
+      'lib/jvent': {
+        exports: 'jvent'
+      },
+      'lib/jplayer.min': {
+        deps: ['lib/jquery']
+      },
+      'lib/prefixfree.jquery': {
+        deps: ['lib/prefixfree.min']
+      },
+          'eic/pluginsniff':{
+                exports: 'pluginsniff'
+          },
+          'lib/base64_handler':{
+                exports: 'base64_handler'
+          },          
+    },
+  });
+
+        require(['eic/PresentationController', 'eic/PresentationController2','eic/PiecesUI','eic/SlideEditor'], function(PresentationController, PresentationController2, PiecesUI, SlideEditor){
+            //var jsonObject,view,controller;
+           // $.getJSON("../data_json/hash_object_test_1.json", function(data){
+            
+            $("#finish").click(function(){
+            	var controller = new PresentationController2(jsonObject);
+	            var view = new PiecesUI(controller);
+	            //controller.init();
+	            console.log("Controller I ", controller);
+	            view.initControls();
+				console.log("Hash Object Output", jsonObject);
+		        controller.once("slide_generation_finished", function(){
+					console.log("Controllers", controller);
+					console.log("Hash Object Output", jsonObject);
+					var editor = new SlideEditor(controller.generator, controller.path, controller, jsonObject);
+		        });
+            });
+            
+        });
+ 
+})(requirejs);
