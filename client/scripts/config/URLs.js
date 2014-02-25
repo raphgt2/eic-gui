84c47d82d7cf35c728e45874e35c076ac2399def
Linda Xu <lindaxu@usc.edu>
2014-02-24 22:25:23 -0800

    URL change to the single path so that it's pointing to local, rather than absolute...makes testing easier

--- client/scripts/config/URLs.js
+++ client/scripts/config/URLs.js
@@ -9,7 +9,7 @@ define(function () {
     festivalspeech: "http://lodstories.isi.edu/festival_service-1.0-SNAPSHOT/rest/audiotest/jsonfull/?jsoncallback=?",
     //paths: "http://pathfinding.restdesc.org/paths",
     paths: "http://lodstories.isi.edu/festival_service-1.0-SNAPSHOT/rest/path/sample/?jsoncallback=?",
-    singlepath: "http://lodstories.isi.edu/festival_service-1.0-SNAPSHOT/rest/path/single/?jsoncallback=?",
+    singlepath: "http://localhost:8080/festival_service-1.0-SNAPSHOT/rest/path/single/?jsoncallback=?",
     jplayerSWF: "/scripts/swf"
   }
 });
