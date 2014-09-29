e304586529fd114dffbbc21eba8e368e1be14ad0
dmaulik <dmaulik93@gmail.com>
2014-09-29 13:28:30 -0700

    deleting server

--- server/start.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-var server = require('./eic-server.js');
-server.start(process.argv[2], { staticFolder: __dirname.replace(/\w+$/, 'client'),
-                                environment: process.argv[3] });
