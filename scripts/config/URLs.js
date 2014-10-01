3b20ab457d0b4108acc9ed8f7f2f3cc6394680b8
Vincent <jianliac@usc.edu>
2014-10-01 10:18:11 -0700

    change to local servlet reference

--- scripts/config/URLs.js
+++ scripts/config/URLs.js
@@ -9,7 +9,7 @@ define(function () {
     //speech: "http://speech.everythingisconnected.be/synthesizer/?jsoncallback=?",
     festivalspeech: "http://lodstories.isi.edu/festival_service-1.0-SNAPSHOT/rest/audiotest/jsonfull/?jsoncallback=?",
     //paths: "http://pathfinding.restdesc.org/paths",
-	ranking: "http://lodstories.isi.edu/LODStories-1.0.0-SNAPSHOT/rankServlet?jsoncallback=?",
+	ranking: "/LODStories/rankServlet?jsoncallback=?",
     singlepath: "http://lodstories.isi.edu/festival_service-1.0-SNAPSHOT/rest/path/single/?jsoncallback=?",
     jplayerSWF: "/scripts/swf"
   }
