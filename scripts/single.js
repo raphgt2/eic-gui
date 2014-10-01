81b1c71a89c8af5de8e9a02a44eea5190bfd083c
Yuting Liu <liu358@usc.edu>
2014-10-01 10:33:53 -0700

    update single.js

--- scripts/single.js
+++ scripts/single.js
@@ -36,11 +36,13 @@
     },
   });
 
+
   require(['eic/PresentationController', 'eic/PiecesUI', 'config/URLs'], function (PresentationController, PiecesUI, urls) {
 		$('#frame').show();
 		
 		$(document).ready(function(){
 			$('#screenWrap').html("<div id='screen' style =' height:465px; width: 758px'> </div>");
+			
 			$.ajax({
                 type: "GET",
                 url: urls.singlepath,
@@ -53,27 +55,16 @@
                 	document.getElementById('subject').innerHTML = path.source.name;
                 	document.getElementById('object').innerHTML = path.destination.name;
                 	var controller = new PresentationController(path, false, false);
-					controller.playMovie();
+					var view = new PiecesUI(controller);
+					view.initControls();
                 }
            });         
 		});
 
 					
         $("#play-button").click(function(){
-			$('#ytholder').html('');
 			$('#screenWrap').html("<div id='screen' style =' height:465px; width: 758px'> </div>");
-			validate(5);
-			//answer[0] = 0; answer[1] = 0; answer[2] = 0; answer[3] = 0;
-			//var mongoClient = new MongoClient(new Server('localhost',27107,{'native_parser':true}));
-			//var db = mongoClient.db('itunes');
-			
-			//app.get('/start', function (req, res) {    
-			//    db.collection('collectionName').insert({req.data}, function (err, doc) {
-			           //rest of code 
-			//    });    
-			//});
-			
-			 $.ajax({
+	        $.ajax({
                 type: "GET",
                 url: urls.singlepath,
                 dataType: "json",
@@ -84,19 +75,20 @@
 					}
 					else{
 						document.getElementById('subject').innerHTML = path.source.name;
-	                	document.getElementById('object').innerHTML = path.destination.name;
+	                	document.getElementById('object').innerHTML = path.destination.name; 
 						var controller = new PresentationController(path, false, false);
 						var view = new PiecesUI(controller);
 						view.initControls();
+						validate(path.source.name,path.destination.name);
 					}
 				},
 				error: function(error){
 					alert("error" + error.status);
 					console.log(error);
 				}
-           });                
+           });   
+	       
+	        
 		 });
-  });
-  
+    });
 })(requirejs);
-
