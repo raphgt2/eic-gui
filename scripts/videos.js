4fbee6a04daf635f3a895f887f69512079281b46
Linda Xu <lindaxu@usc.edu>
2015-02-18 02:16:09 -0800

    Fixed the replay issue...Needed to detach() the outroButtons before wiping the screen's html so that the event bindings would be preserved

--- scripts/videos.js
+++ scripts/videos.js
@@ -102,6 +102,7 @@
 					})
 					
 					$(exitButtons[2]).click(function () {
+						$('.navigation').detach();
 						$('#screen').html('');
 						$('#subtitles').text('');
 						$('#screenWrap').show();
